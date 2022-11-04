044448d141db544ba0f47b2684740a9c6b697d30
DJ Drakos <dianajodrakos@gmail.com>
2022-11-04 15:54:12 -0700

    add responsive font styles

--- src/components/Styled.js
+++ src/components/Styled.js
@@ -1,59 +1,125 @@
 import styled from 'styled-components';
+import breakpoints from '../styles/breakpoints';
 
 const H1 = styled.h1`
-  display: inline-block;
   font-size: 12.5rem;
   font-weight: 700;
   line-height: 1;
   letter-spacing: -.025rem;
   text-transform: capitalize;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: 10.9375rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: 6.25rem;
+  }
 `
 
 const H2 = styled.h2`
   font-size: 3.5rem;
   font-weight: 700;
-  line-height: calc(68/56);
+  line-height: 4.25rem;
   text-transform: capitalize;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: 2.5rem;
+    line-height: 3rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: 1.5rem;
+  }
 `
 
 const H3 = styled.h3`
   font-size: 1.5rem;
   font-weight: 700;
-  line-height: calc(28/24);
+  line-height: 1.75rem;
   text-transform: uppercase;
   letter-spacing: .3rem;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: 1.125rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .9375rem;
+  }
 `
 
 const H4 = styled.h4`
   font-size: 1.25rem;
   font-weight: 400;
-  line-height: calc(28/20);  
+  line-height: 1.75rem;  
   text-transform: uppercase;
   letter-spacing: .25rem;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: 1.125rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .9375rem;
+  }
 `
 
 const H5 = styled.h5`
   margin-block-start: .8125rem;
   font-size: 1.125rem;
   font-weight: 700;
-  line-height: calc(28/18);  
+  line-height: 1.75rem;  
   text-transform: capitalize;
   font-style: normal;
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .75rem;
+    line-height: 1.375rem;
+  }
 `
 
 const H6 = styled.h6`
   font-size: .9375rem;
   font-weight: 400;
   text-transform: uppercase;
-  line-height: calc(28/15);
+  line-height: 1.75rem;
   letter-spacing: .1875rem;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: .8125rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .625rem;
+  }
 `
 
 const P = styled.p`
   font-weight: 400;
   font-size: 1.125rem;
-  line-height: calc(28/18);
+  line-height: 1.75rem;
   text-transform: capitalize;
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .75rem;
+    line-height: 1.375rem;
+  }
 `
 
-export { H1, H2, H3, H4, H5, H6, P } 
+  const Timezone = styled.span`
+  font-size: 2.5rem;
+  font-weight: 400;  
+  text-transform: uppercase;
+  letter-spacing: .25rem;
+
+  @media screen and ${breakpoints.tablet} {
+    font-size: 2rem;
+  }
+
+  @media screen and ${breakpoints.mobile} {
+    font-size: .9375rem;
+  }
+`
+
+export { H1, H2, H3, H4, H5, H6, P, Timezone } 
