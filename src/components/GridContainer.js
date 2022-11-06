987691716d18d49652af465be8e0fa095b1ff647
DJ Drakos <dianajodrakos@gmail.com>
2022-11-05 20:10:25 -0700

    improve GridContainer responsive column sizing

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -4,23 +4,23 @@ import breakpoints from '../styles/breakpoints'
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(12, minmax(4rem, 1fr));
-  padding-inline: max(10rem, 12vw);
-  gap: 1.875rem;
+  padding-inline: max(8rem, 12vw);
+  gap: .625rem;
 
   @media screen and ${breakpoints.tabletLg} {
-    grid-template-columns: repeat(12, minmax(3.125rem, 1fr));
+    grid-template-columns: repeat(12, minmax(3.125rem, 4rem));
     gap: .625rem;
-    padding-inline: max(4rem, 12vw);
+    padding-inline: max(6rem, 10vw);
   }
 
   @media screen and ${breakpoints.tabletSm} {
-    grid-template-columns: repeat(12, minmax(3.125rem, 1fr));
-    gap: .25rem;
+    grid-template-columns: repeat(12, minmax(2rem, 4rem));
+    gap: .625rem;
     padding-inline: max(4rem, 8vw);
   }
 
   @media screen and ${breakpoints.mobile} {
-    grid-template-columns: repeat(12, minmax(1.5rem, 1fr));
+    grid-template-columns: repeat(12, minmax(1.25rem, 1fr));
     gap: .1875rem;
     padding-inline: max(1.625rem, 6vw);
   }
