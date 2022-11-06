a935a45ea6ae93011ce2d8dc02b9f917ead8da77
DJ Drakos <dianajodrakos@gmail.com>
2022-11-05 17:47:15 -0700

    add basic toggle function

---
+++ src/components/ToggleDetails.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { ReactComponent as IconArrowDown } from '../assets/desktop/icon-arrow-down.svg'
+import { ReactComponent as IconArrowUp } from '../assets/desktop/icon-arrow-up.svg'
+import breakpoints from '../styles/breakpoints';
+
+const Styled = styled.button`
+  --arrowBgSize: 2.5rem;
+  display: grid;
+  grid-template-columns: 1fr var(--arrowBgSize);
+  justify-items: center;
+  height: 3.5rem;
+  width: 9.125rem;
+  color: var(--grey-mid);
+  background: var(--white);
+  border-radius: 28px;
+  font-weight: 700;
+  font-size: 1rem;
+  text-transform: uppercase;
+  letter-spacing: .3125rem;
+  padding: .5rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--grey-dark);
+    border-radius: 50%;
+    height: var(--arrowBgSize);
+    width: var(--arrowBgSize);
+  }
+
+  :hover {
+    span {
+      background-color: var(--grey-light);
+    }
+  }
+  
+  @media screen and ${breakpoints.mobile} {
+    --arrowBgSize: 2rem;
+    height: 2.5rem;
+    width: 7.1875rem;
+    font-size: .75rem;
+    letter-spacing: .234rem;
+    line-height: .875rem;
+    padding: .25rem;
+  }
+`
+
+export default function ToggleDetails({detailsIsOpen, toggleDetails}) {
+  return (
+    <Styled onClick={toggleDetails}>
+      { !detailsIsOpen
+      ? <>More 
+          {/* <span> */}
+            <IconArrowDown />
+          {/* </span> */}
+        </> 
+      : <> Less 
+          {/* <span> */}
+            <IconArrowUp />
+          {/* </span> */}
+      </> }
+    </Styled>
+  )
+}
