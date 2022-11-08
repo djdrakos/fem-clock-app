f8f81dc63958e1e6daa9d1d99b4ec7e1b83111a8
DJ Drakos <dianajodrakos@gmail.com>
2022-11-08 15:28:53 -0800

    fetch and render random quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
-import { H5, P } from './Typography';
+import { useEffect, useState } from 'react'
 import { ReactComponent as IconRefresh } from '../assets/desktop/icon-refresh.svg'
 import GridContainer from './GridContainer';
 import GridItem from './GridItem';
+import { H5, P } from './Typography';
 
 const StyledFig = styled.figure`
   .flex {
@@ -30,20 +31,41 @@ const StyledFig = styled.figure`
 `
 
 export default function Quote() {
+  const [ quote, setQuote ] = useState(null);
+  
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const res = await fetch("https://programming-quotes-api.herokuapp.com/Quotes/random");
+      const quote = await res.json()
+      setQuote(quote)
+    }
+    fetchQuote()
+  }, [])
+
+  const handleRefreshQuote = () => {
+
+  }
+
   return (
       <StyledFig className='quote'>
         <GridContainer>
           <GridItem s={12} m={11} l={7} xl={6} className='flex'>
             <div className="wrapper">
-              <P as="blockquote">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porta et diam in vestibulum. Aliquam nunc enim, condimentum in volutpat viverra, ultricies non sem.
-              </P>
-              <H5 as="figcaption">
-                Phasellus eu Nisi Mauris
-              </H5>
+              { quote === null
+              ? <p>Loading Quote...</p>
+              : 
+              <>
+                <P as="blockquote">
+                  {quote?.en}
+                </P>
+                <H5 as="figcaption">
+                  {quote?.author}
+                </H5>
+              </>
+              }
             </div>
 
-            <button>
+            <button onClick={handleRefreshQuote}>
               <IconRefresh />
             </button>
           </GridItem>
