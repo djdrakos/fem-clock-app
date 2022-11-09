b818caeabff20ad0ed40f12d6dfb0230ed405e2d
DJ Drakos <dianajodrakos@gmail.com>
2022-11-09 11:37:57 -0800

    format day of the week for rendering

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,4 +1,4 @@
-import { formatTimezoneLocation } from './formatUtils';
+import { formatDayOfWeek, formatTimezoneLocation } from './formatUtils';
 
 export const fetchRandomQuote = async () => {
   const res = await fetch("https://programming-quotes-api.herokuapp.com/Quotes/random");
@@ -14,7 +14,7 @@ export const fetchCurrentTimeData = async () => {
   const res = await fetch("http://worldtimeapi.org/api/ip")
   const body = await res.json()
   const timeData = {
-    dayOfWeek: body.day_of_week,
+    dayOfWeek: formatDayOfWeek(body.day_of_week),
     dayofYear: body.day_of_year,
     startTime: body.unixtime,
     timezone: formatTimezoneLocation(body.timezone),
--- src/utils/formatUtils.js
+++ src/utils/formatUtils.js
@@ -2,3 +2,16 @@ export function formatTimezoneLocation(string) {
   const [country, city] = string.replaceAll('_', ' ').split('/')
   return { city, country }
 }
+
+export function formatDayOfWeek(number) {
+  const daysOfTheWeek = {
+    1: 'Mondau',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday'
+  }
+  return daysOfTheWeek[number]
+}
